e1f6bf294694576b9632740cf2cc5a6faa7be684
WolleKa <wolle.kaiser84@googlemail.com>
2021-04-21 21:34:21 +0200

    Added commands for switching views

--- KronosUI/ViewModels/ControlViewModel.cs
+++ KronosUI/ViewModels/ControlViewModel.cs
@@ -11,28 +11,84 @@ namespace KronosUI.ViewModels
 {
     public class ControlViewModel : BindableBase
     {
+        public enum ViewState { Configuration = 0, WeekListing, MonthListing, YearListing };
+
+        private ViewState currentState = ViewState.Configuration;
+
         public ControlViewModel()
         {
             SwitchToConfigurationViewCommand = new DelegateCommand(SwitchToConfigurationView, CanSwitchToConfigurationView);
+            SwitchToWeekListingViewCommand = new DelegateCommand(SwitchToWeekListingView, CanSwitchToWeekListingView);
+            SwitchToMonthListingViewCommand = new DelegateCommand(SwitchToMonthListingView, CanSwitchToMonthListingView);
+            SwitchToYearListingViewCommand = new DelegateCommand(SwitchToYearListingView, CanSwitchToYearListingView);
         }
 
         #region Command functions
 
         void SwitchToConfigurationView()
         {
-            MessageBox.Show("Hello world");
+            CurrentState = ViewState.Configuration;
         }
 
         bool CanSwitchToConfigurationView()
         {
-            return true;
+            return CurrentState != ViewState.Configuration;
+        }
+
+        void SwitchToWeekListingView()
+        {
+            CurrentState = ViewState.WeekListing;
+        }
+
+        bool CanSwitchToWeekListingView()
+        {
+            return CurrentState != ViewState.WeekListing;
+        }
+
+        void SwitchToMonthListingView()
+        {
+            CurrentState = ViewState.MonthListing;
+        }
+
+        bool CanSwitchToMonthListingView()
+        {
+            return CurrentState != ViewState.MonthListing;
+        }
+
+        void SwitchToYearListingView()
+        {
+            CurrentState = ViewState.YearListing;
+        }
+
+        bool CanSwitchToYearListingView()
+        {
+            return CurrentState != ViewState.YearListing;
         }
 
         #endregion
 
         #region Properties
 
+        public ViewState CurrentState
+        {
+            get { return currentState; }
+            set
+            {
+                SetProperty(ref currentState, value);
+                SwitchToConfigurationViewCommand.RaiseCanExecuteChanged();
+                SwitchToWeekListingViewCommand.RaiseCanExecuteChanged();
+                SwitchToMonthListingViewCommand.RaiseCanExecuteChanged();
+                SwitchToYearListingViewCommand.RaiseCanExecuteChanged();
+            }
+        }
+
         public DelegateCommand SwitchToConfigurationViewCommand { get; private set; }
+
+        public DelegateCommand SwitchToWeekListingViewCommand { get; private set; }
+
+        public DelegateCommand SwitchToMonthListingViewCommand { get; private set; }
+
+        public DelegateCommand SwitchToYearListingViewCommand { get; private set; }
 
         #endregion
     }
