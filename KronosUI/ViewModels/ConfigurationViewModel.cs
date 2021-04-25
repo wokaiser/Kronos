2333515cebc08298af073572ff85aecd82c2fc87
WolleKa <wolle.kaiser84@googlemail.com>
2021-04-25 17:20:26 +0200

    Improved TrreeView for Accounting

--- KronosUI/ViewModels/ConfigurationViewModel.cs
+++ KronosUI/ViewModels/ConfigurationViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using KronosData.Logic;
 using KronosData.Model;
+using Prism.Commands;
 using Prism.Ioc;
 using Prism.Mvvm;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace KronosUI.ViewModels
 {
     public class ConfigurationViewModel : BindableBase
     {
         private ObservableCollection<Account> currentAccounts;
+        private object selectedItem;
 
         private DataManager dataManger;
 
@@ -16,7 +20,30 @@ namespace KronosUI.ViewModels
         {
             dataManger = ContainerLocator.Container.Resolve<DataManager>();
             CurrentAccounts = dataManger.Accounts;
+
+            InitializeCommands();
         }
+
+        #region Command functions
+
+        private void InitializeCommands()
+        {
+            ItemSelectionChangedCommand = new DelegateCommand<object>(ItemSelectionChanged);
+        }
+
+        private void ItemSelectionChanged(object param)
+        {
+            if (param is Account)
+            {
+                MessageBox.Show("Account Nr: " + (param as Account).Number + " selected");
+            }
+            else if (param is WorkTask)
+            {
+                MessageBox.Show("Task: " + (param as WorkTask).Title + " selected");
+            }
+        }
+
+        #endregion
 
         #region Properties
 
@@ -28,6 +55,17 @@ namespace KronosUI.ViewModels
                 SetProperty(ref currentAccounts, value);
             }
         }
+
+        public object SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                SetProperty(ref selectedItem, value);
+            }
+        }
+
+        public ICommand ItemSelectionChangedCommand { get; private set; }
 
         #endregion
     }
