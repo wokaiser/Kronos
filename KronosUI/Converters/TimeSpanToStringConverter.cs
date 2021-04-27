12f4fe0cc214775db2a09d6fdd623281d9e59f27
WolleKa <wolle.kaiser84@googlemail.com>
2021-04-28 01:06:06 +0200

    Updated viewmodel for WeekListingView

---
+++ KronosUI/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace KronosUI.Converters
+{
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TimeSpan))
+            {
+                throw new ArgumentException(string.Format("{0} is not from type {1}", nameof(value), nameof(TimeSpan)));
+            }
+
+            if (((TimeSpan)value).Ticks == 0)
+            {
+                return string.Empty;
+            }
+
+            return ((TimeSpan)value).ToString(@"hh\:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
