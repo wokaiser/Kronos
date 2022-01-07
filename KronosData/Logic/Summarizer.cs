78b2daaef345e173e0804f2f038a06156dde035c
WolleKa <wolle.kaiser84@googlemail.com>
2022-01-07 20:03:16 +0100

    Moved files

--- KronosData/Logic/Summarizer.cs
+++ KronosData/Logic/Summarizer.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using KronosData.Model;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-namespace KronosData.Model
+namespace KronosData.Logic
 {
     public static class Summarizer
     {
