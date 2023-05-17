6dae8804b9d4d21f84f509468aee460570f19315
Wolfgang <wolle.kaiser84@googlemail.com>
2023-05-17 19:54:53 +0200

    Added color conversion for negative times

---
+++ KronosUI/Converters/ValueToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace KronosUI.Converters
+{
+    public class ValueToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value as string;
+
+            if (val == null || val[0] != '-')
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+
+            return new SolidColorBrush(Colors.DarkRed);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
