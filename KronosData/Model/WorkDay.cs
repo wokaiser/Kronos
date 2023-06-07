3c684aa2934be6143d5bf8c45c9cb077c271f040
Wolfgang <wolle.kaiser84@googlemail.com>
2023-06-08 01:09:15 +0200

    Fixed json ignore bug caused by using wrong reference

--- KronosData/Model/WorkDay.cs
+++ KronosData/Model/WorkDay.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.ObjectModel;
 using System.Text;
-using System.Text.Json.Serialization;
 
 namespace KronosData.Model
 {
